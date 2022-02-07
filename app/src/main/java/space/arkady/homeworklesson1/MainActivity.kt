351e2ccf59683e24e9877a7af36f410085182fac
Аркадий <arkady.galperin@gmail.com>
2022-02-07 19:12:54 +0300

    re

--- app/src/main/java/space/arkady/homeworklesson1/MainActivity.kt
+++ app/src/main/java/space/arkady/homeworklesson1/MainActivity.kt
@@ -1,21 +1,12 @@
 package space.arkady.homeworklesson1
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val buttonSub by lazy { findViewById<Button>(R.id.btnShow) }
-    private val firstName by lazy { findViewById<EditText>(R.id.inputFirstName) }
-    private val secondName by lazy { findViewById<EditText>(R.id.inputSecondName) }
-    private val setAge by lazy { findViewById<EditText>(R.id.inputAge) }
-    private val setuserStat by lazy { findViewById<TextView>(R.id.UserView) }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +21,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        buttonSub.setOnClickListener {
-            val name = firstName.text.toString()
-            val secondName = secondName.text.toString()
+        btnShow.setOnClickListener {
+            val name = inputFirstName.text.toString()
+            val secondName = inputSecondName.text.toString()
 
-            val age: Int = setAge.text.toString().toInt()
+            val age: Int = inputAge.text.toString().toInt()
             if (name.isEmpty() || secondName.isEmpty()) showToast() else newUser(name, secondName, age)
         }
     }
@@ -46,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     private fun newUser(name: String, secondName: String, age: Int) {
         val user = User(name, secondName, age)
         val resultText = "${user.name}, ${user.secondName}, ${user.age}"
-        setuserStat.text = resultText
+        UserView.text = resultText
 
 
     }
